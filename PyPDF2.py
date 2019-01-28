33499512f7868b8396a2b93c5b76964a3c54f508
ysli2018 <46211347+ysli2018@users.noreply.github.com>
2019-01-28 16:14:26 -0500

    Add files via upload

---
+++ PyPDF2.py
@@ -0,0 +1,25 @@
+import os
+print(os.getcwd())
+os.chdir("...")
+
+from PyPDF2 import PdfFileReader
+
+# Note there is no space between words extracted
+fileObj = open("2019-01-20.pdf", "rb")
+pdfreader = PdfFileReader(fileObj)
+
+# Check individual page(s)
+page1 = pdfreader.getPage(1)
+print(page1.extractText())
+
+# Define a function to read all pages (or page range)
+def get_text(path):
+    # Load PDF into PyPDF2
+    pdfreader = PdfFileReader(open(path, "rb"))
+    # Iterate over all pages
+    content = ""
+    for i in range(0, pdfreader.getNumPages()):
+        content += pdfreader.getPage(i).extractText() + "\n"
+    return content
+
+get_text("2019-01-20.pdf")
